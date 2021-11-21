a3e18b7dacbdf23d1e45a040ae90616b9c11bda5
Nhat Nguyen <nhat12082000@gmail.com>
2021-11-22 01:05:45 +0700

    Change sql table car

--- database/car-table.sql
+++ database/car-table.sql
@@ -2,7 +2,9 @@ CREATE TABLE Car(
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 	name VARCHAR(50) NOT NULL, 
 	brand VARCHAR(30), 
-	year INT(4) UNSIGNED, seats INT, color VARCHAR(30), 
+	year INT(4) UNSIGNED, 
+	seats INT, 
+	color VARCHAR(30), 
 	tranmisstion VARCHAR(10), # manual, auto 
 	price DECIMAL(15,2), 
 	description TEXT 
