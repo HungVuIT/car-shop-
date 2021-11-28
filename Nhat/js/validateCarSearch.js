7553c9f9c2dea959b0770d1db28e783aee64224a
Nhat Nguyen <nhat12082000@gmail.com>
2021-11-28 10:23:31 +0700

    Remove validateCarSearch.js. Move it's function to liveSearch.js

--- Nhat/js/validateCarSearch.js
+++
@@ -1,6 +0,0 @@
-function validateCarSearch() {
-    var result = document.getElementById("search-keyword").value;
-    if(result.length > 0){
-        window.location.href='carResult.php?keyword='+ result;
-    }
-}
