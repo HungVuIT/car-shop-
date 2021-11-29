9415962e1af9e69499e9a6c9d5ede18717511e7b
HuuHanh31 <91646453+HuuHanh31@users.noreply.github.com>
2021-11-29 11:08:40 +0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,71 @@
               <a class="nav-link" href="news.php">News</a>
             </li>
             <li class="nav-item">
+              <?php if(isset($_SESSION['id'])) :
+              ?>  
+                <div class="account-wrap">
+                  <div class="account-item clearfix js-item-menu">
+                      <?php 
+                          $sql = "SELECT * FROM user WHERE id='{$_SESSION["id"]}'";
+                          $result = mysqli_query($conn, $sql);
+                          if (mysqli_num_rows($result) > 0) {
+                              while ($row = mysqli_fetch_assoc($result)) {
+                      ?>
+
+                      <div class="image">
+                          <?php if (empty($row['img_path'])) : ?>
+                              <img src="img/user/default_avatar.png" alt="<?=$row['name'];?>"/>
+                          <?php else : ?>
+                              <img src="<?php echo 'img/user' . $row['img_path'] ?>" alt="avatar">
+                          <?php endif; ?>    
+                      </div>    
+
+                      <div class="content">
+                          <a class="js-acc-btn" href="#"><?=$row['name'];?></a>
+                      </div>
+
+                      <div class="account-dropdown js-dropdown">
+                          <div class="info clearfix">
+                              <div class="image">
+                                  <a href="profile.php">
+                                      <?php if (empty($row['img_path'])) : ?>
+                                          <img src="img/user/default_avatar.png" alt="<?=$row['name'];?>"/>
+                                      <?php else : ?>
+                                          <img src="<?php echo 'img/user' . $row['img_path'] ?>" alt="avatar">
+                                      <?php endif; ?>
+                                  </a>
+                              </div>
+                              <div class="content">
+                                  <h5 class="name">
+                                      <a href="profile.php"><?=$row['name'];?></a>
+                                  </h5>
+                                  <span class="email"><?=$row['email'];?></span>
+                              </div>
+                          </div>
+                      <?php
+                          }
+                      }
+                      ?>
+                          <div class="account-dropdown__body">
+                              <div class="account-dropdown__item">
+                                  <a href="#">
+                                      <i class="zmdi zmdi-account"></i>Account</a>
+                              </div>
+                              <div class="account-dropdown__item">
+                                  <a href="#">
+                                      <i class="fas fa-box"></i>Order</a>
+                              </div>
+                          </div>
+                          <div class="account-dropdown__footer">
+                              <a href="logout.php">
+                              <i class="zmdi zmdi-power"></i>Logout</a>
+                          </div>
+                      </div>
+                  </div>
+                </div>
+            <?php else : ?>
               <a class="nav-link" href="login.php">Login</a>
+            <?php endif; ?>
             </li>
           </ul>
         </div>
